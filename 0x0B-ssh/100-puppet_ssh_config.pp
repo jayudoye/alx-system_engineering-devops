9bf992710cb8d33abd31f8c66dcfca86e3effee9
jayudoye <joeludoye@gmail.com>
2024-06-30 17:32:00 -0700

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,11 @@
 # connect with puppet
 
 file_line { 'Declare_identity_file':
-  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn_off_passwd_auth':
-  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
 }
