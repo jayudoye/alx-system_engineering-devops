4cfeb1cf122a21b13ba44a646043fffe66994f13
jayudoye <joeludoye@gmail.com>
2024-07-02 05:11:39 -0700

    SSH Advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
 # uses puppet to connect and make changes to local ssh config file
+include 'stdlib'
 
 file_line {'Turn off passwd auth':
   ensure => present,
-  path   => '/home/.ssh/config',
+  path   => '/etc/ssh/config',
   line   => 'PasswordAuthentication no',
 }
 
 file_line {'Declare identity file':
   ensure => present,
-  path   => '/home/.ssh/config',
-  line   => 'IdentityFile /home/.ssh/school',
+  path   => '/etc/ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
